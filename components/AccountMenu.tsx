3216b34ceaeb93a14df6942b19ef8928bc664dd2
Kamil <kamil.nader1@gmail.com>
2023-09-03 19:39:39 +0200

    Added search icon and notification icon

---
+++ components/AccountMenu.tsx
@@ -0,0 +1,12 @@
+import { signOut } from "next-auth/react";
+import React from "react";
+
+interface AccountMenuProps {
+  visible?: boolean;
+}
+
+const AccountMenu = () => {
+  return <div></div>;
+};
+
+export default AccountMenu;
